faca5f76cbefa9a3967bf659a0837d2247644017
fernandes-glen <g.hensonfernandes@elsevier.com>
2023-10-11 11:55:37 +0530

    I  Have added a first route to go to that web page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import About from "./components/pages/About";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
