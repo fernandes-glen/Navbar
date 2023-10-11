e1ee20509dc4d3544cd5fbd385134087a1272f30
fernandes-glen <g.hensonfernandes@elsevier.com>
2023-10-11 10:03:33 +0530

    I  have added router package

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav>
       <ul>
-        <li>About</li>
-        <li>Services</li>
-        <li>Contact</li>
+        <li>
+          <Link>About</Link>
+        </li>
+        <li>
+          <Link>Services</Link>
+        </li>
+        <li>
+          <Link>Contact</Link>
+        </li>
       </ul>
     </nav>
   );
