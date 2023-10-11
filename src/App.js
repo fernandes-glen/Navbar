c8359ed3827f93ea9379290d1fde2bd2e173f5c3
fernandes-glen <g.hensonfernandes@elsevier.com>
2023-10-11 12:02:09 +0530

    I  Have given route to all the pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Navbar from "./components/Navbar";
 import About from "./components/pages/About";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import Contact from "./components/pages/Contact";
+import Services from "./components/pages/Services";
+import Home from "./components/pages/Home";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </div>
   );
